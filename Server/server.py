a02b49e8249569dba00fa5abf7afd61699066b87
NOOB2033 <zaza322228@mail.ru>
2023-04-09 10:55:28 +0700

    remove flash from server

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from models import getResults
 
